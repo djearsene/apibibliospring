4e5ea106e87d10a6b8fa0287f7b037527375b477
djearsene <ouanyou21@gmail.com>
2026-03-28 08:36:34 +0100

    Premier commit : projet Spring Boot api-bibliotheque

---
+++ src/main/java/com/bibliotheque/api_bibliotheque/ApiBibliothequeApplication.java
@@ -0,0 +1,13 @@
+package com.bibliotheque.api_bibliotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBibliothequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBibliothequeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bibliotheque/api_bibliotheque/ApiBibliothequeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliotheque.api_bibliotheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBibliothequeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
